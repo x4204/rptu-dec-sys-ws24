5818a6b3e7d9d450dd03affe92ae4f9511bc5fdc
Maksim Rastorguev <rastorguev10@gmail.com>
2025-02-08 01:18:00 +0100

    avg_and_total

--- stats/plot.py
+++ stats/plot.py
@@ -73,12 +73,49 @@ def parse_stat_line(line):
 def read_stats(path):
     stats = collections.defaultdict(list)
 
+    prev_stat = {}
+    buckets = {}
+    totals = {}
+
     with open(path) as file:
         for line in file:
             stat = parse_stat_line(line)
-            stats[stat['name']].append(stat)
+            
+            if not prev_stat.get(stat['name']):
+                prev_stat[stat['name']] = stat
+                buckets[stat['name']] = {}
+                buckets[stat['name']]['cpu_usage'] = []
+                buckets[stat['name']]['mem_usage'] = []
+                buckets[stat['name']]['blk_read'] = []
+                buckets[stat['name']]['blk_write'] = []
+                buckets[stat['name']]['net_read'] = []
+                buckets[stat['name']]['net_write'] = []
 
-    return stats
+            if (stat['ts'] - prev_stat[stat['name']]['ts']).total_seconds() > 10:
+                buckets[stat['name']]['blk_read'].append(stat['blk_read']-prev_stat[stat['name']]['blk_read'])
+                buckets[stat['name']]['blk_write'].append(stat['blk_write']-prev_stat[stat['name']]['blk_write'])
+                buckets[stat['name']]['net_write'].append(stat['net_write']-prev_stat[stat['name']]['net_write'])
+                buckets[stat['name']]['net_read'].append(stat['net_read']-prev_stat[stat['name']]['net_read'])
+
+                prev_stat[stat['name']] = stat
+            buckets[stat['name']]['cpu_usage'].append(stat['cpu_usage'])
+            buckets[stat['name']]['mem_usage'].append(stat['mem_usage'])
+
+            stats[stat['name']].append(stat)
+    
+    for node in sorted(stats.keys()):
+        buckets[node]['blk_read'].append(stats[node][-1]['blk_read']-prev_stat[node]['blk_read'])
+        buckets[node]['blk_write'].append(stats[node][-1]['blk_write']-prev_stat[node]['blk_write'])
+        buckets[node]['net_read'].append(stats[node][-1]['net_read']-prev_stat[node]['net_read'])
+        buckets[node]['net_write'].append(stats[node][-1]['net_write']-prev_stat[node]['net_write'])
+
+        totals[node] = {}
+        totals[node]['blk_read'] = stats[node][-1]['blk_read']
+        totals[node]['blk_write'] = stats[node][-1]['blk_write']
+        totals[node]['net_write'] = stats[node][-1]['net_write']
+        totals[node]['net_read'] = stats[node][-1]['net_read']
+    
+    return (stats, buckets, totals)
 
 
 def main():
@@ -88,7 +125,25 @@ def main():
 
     _, in_path, out_path = sys.argv
 
-    stats = read_stats(in_path)
+    stats, buckets, totals = read_stats(in_path)
+
+    with open(f'{out_path}/avg_and_total.txt', 'w') as file:
+        for node in sorted(buckets.keys()):
+            file.write(f'{node}\n')
+            file.write('Average values:\n')
+            file.write(f'avg_cpu_usage: {sum(buckets[node]['cpu_usage'])/len(buckets[node]['cpu_usage'])}\n')
+            file.write(f'avg_mem_usage: {sum(buckets[node]['mem_usage'])/len(buckets[node]['mem_usage'])}\n')
+            file.write(f'avg_blk_read: {sum(buckets[node]['blk_read'])/len(buckets[node]['blk_read'])}\n')
+            file.write(f'avg_blk_write: {sum(buckets[node]['blk_write'])/len(buckets[node]['blk_write'])}\n')
+            file.write(f'avg_net_read: {sum(buckets[node]['net_read'])/len(buckets[node]['net_read'])}\n')
+            file.write(f'avg_net_write: {sum(buckets[node]['net_write'])/len(buckets[node]['net_write'])}\n\n')
+
+            file.write('Total values:\n')
+            file.write(f'total_blk_read: {totals[node]['blk_read']}\n')
+            file.write(f'total_blk_write: {totals[node]['blk_write']}\n')
+            file.write(f'total_net_read: {totals[node]['net_read']}\n')
+            file.write(f'total_net_write: {totals[node]['net_write']}\n\n')
+
     assert len(stats) <= len(COLORS), f'{len(stats)} > {len(COLORS)}'
 
     metrics = [
